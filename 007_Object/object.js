ff89e7acfb493f874b2f2c1929abf95c3573aa56
Shameem Ahammed <shameem105455@gmail.com>
2024-07-30 11:48:08 +0600

    object complete

--- 007_Object/object.js
+++ 007_Object/object.js
@@ -49,14 +49,14 @@ car.price = "12";
 
 console.log(car);
 
-// ? if a key exist or not. how to check
+//  if a key exist or not. how to check
 console.log(Person.hasOwnProperty('address'));
 Person.address ='Dhaka Bangladesh';
 console.log(Person.hasOwnProperty('address'));
 
-// ? find the keys in an object
+//  find the keys in an object
 console.log(Object.keys(Person));
-// ? values
+//  values
 console.log(Object.values(Person));
 
 
@@ -78,7 +78,7 @@ const obj3 ={
 // const objFinal = { obj1, obj2};
 // console.log(objFinal);
 // * Object.assign(target, soruce);
-// const objFinal = Object.assign(obj1, obj2, obj3);
+// const objFinal = Object.assign(obj1, obj2, obj3); --> not memory efficient. all merge to obj1
 // console.log(objFinal);
 console.log(obj1); //obj1 == objFinal
 
@@ -99,7 +99,6 @@ const obj = {...obj1, ...obj2, ...obj4}
 console.log(obj);
 
 // object inside object
-
 const outsideObject = {
     fullName: 'Shameem Ahammed',
     address: {
@@ -114,4 +113,35 @@ const x = outsideObject
 x.fullName = "Mr x"
 console.log(outsideObject);
 
+
+// Constructor Object
+/*
+* used to create many object of same type
+* not memory efficient. use prototype instead
+*/
+
+function Students(firstName, age){
+    this.firstName = firstName,
+    this.age = age
+};
+const student1 = new Students('Shameem', 8);
+const student2 = new Students("Ahammed", 56);
+console.log(student1);
+console.log(student2);
+// change value & add property to object
+student1.age = 34;
+student1.nationality = 'Bangladeshi';
+console.log(student1);
+console.log(student2);
+
+// add property to constructor
+Students.prototype.university = 'Kuet';
+console.log(student2.university);
+
+// add methods to constructor
+Students.prototype.changeName = function(name){
+    this.firstName = name;
+};
+student1.changeName('no name');
+console.log(student1);
 
